5db425514d289eba4b9dd423deec9c6f397e6405
Pradeep Daka <pradeepkumardaka@sbpcorp.in>
2025-09-16 06:49:03 +0000

    adding navigation changes

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -494,6 +494,25 @@ sap.ui.define([
                     existingDiv.removeChild(existingDiv.firstChild);
                 }
             }
-        }
+        },
+        onNavPress: function () {
+            if (sap.ushell && sap.ushell.Container && sap.ushell.Container.getService) {
+                var oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation");
+                // generate the Hash to display 
+                var hash = (oCrossAppNavigator && oCrossAppNavigator.hrefForExternal({
+                    target: {
+                        semanticObject: "VCPDocument",
+                        action: "Display"
+                    }
+                })) || "";
+                var oStorage = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+                oStorage.put("nodeId", 103);
+                //Generate a  URL for the second application
+                var url = window.location.href.split('#')[0] + hash;
+                //Navigate to second app
+                sap.m.URLHelper.redirect(url, true);
+            }
+        },
+
     });
 });
